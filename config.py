7e53f91e0f07951b2b747a7fb5e7047831441e65
Resindra Aji <resindraburiza@gmail.com>
2021-01-29 23:54:04 +0900

    modify config

--- config.py
+++ config.py
@@ -10,3 +10,6 @@ class DefaultConfig:
     PORT = 3978
     APP_ID = os.environ.get("MicrosoftAppId", "")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+
+    APP_ID_DEPLOY = 'bbf340f5-a48e-4a57-ba70-a0825e59202d'
+    APP_PASSWORD_DEPLOY = 'aac364bd-32ca-4484-ab4f-7ecc92a22f7b'
