744ad1df17d3be108326b8965eb03a0745e14a37
Resindra Aji <resindraburiza@gmail.com>
2021-01-29 23:27:27 +0900

    restructure folder

--- basic_bot/state_management/__init__.py
+++
@@ -1,4 +0,0 @@
-from .conversation_data import ConversationData
-from .user_profile import UserProfile
-
-__all__ = ["ConversationData", "UserProfile"]
--- basic_bot/state_management/user_profile.py
+++
@@ -1,4 +0,0 @@
-class UserProfile:
-    def __init__(self, student_name=None, student_id=None):
-        self.student_name = student_name
-        self.student_id = student_id
